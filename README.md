5fc26cf3c53c8ad563dbabf2e40a3cbbfc41a390
KalilDev <kalilgamer0@gmail.com>
2021-10-21 13:52:07 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Shape theme switcher
+
+Provides the ShapeThemeSwitcher widget, which takes an theme, an alignment and
+an shape, and once the theme changes, it builds the child with the new theme,
+takes the image of the current child, and displays the new theme under the old,
+while clipping the old to the shape, growing until it fills the screen and only
+the new child is visible.
+
+
+## DEMO
+![Video of the package](assets/demo.gif)
---
+++ lib/shape_theme_switcher.dart
@@ -0,0 +1,154 @@
+import 'dart:math' as math;
+
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'dart:ui' as ui;
+
+class ShapeThemeSwitcher extends StatefulWidget {
+  final ThemeData theme;
+  final Widget child;
+  final Alignment alignment;
+  final ShapeBorderTween borderTween;
+  ShapeThemeSwitcher({
+    Key? key,
+    required this.theme,
+    required this.child,
+    ShapeBorderTween? borderTween,
+    ShapeBorder? border = const CircleBorder(),
+    this.alignment = Alignment.bottomRight,
+  })  : borderTween =
+            borderTween ?? ShapeBorderTween(begin: border!, end: border),
+        super(key: key);
+
+  @override
+  _ShapeThemeSwitcherState createState() => _ShapeThemeSwitcherState();
+}
+
+class _ShapeThemeSwitcherState extends State<ShapeThemeSwitcher>
+    with SingleTickerProviderStateMixin {
+  final _boundaryKey = GlobalKey();
+  late ThemeData theme;
+  ui.Image? oldThemedImage;
+  late final _controller = AnimationController(
+    vsync: this,
+    duration: Duration(milliseconds: 1000),
+  )..addStatusListener(_onAnimationStatus);
+  late final _animation = CurvedAnimation(
+    parent: _controller,
+    curve: Curves.easeInOut,
+  );
+
+  void _onAnimationStatus(AnimationStatus status) {
+    if (status != AnimationStatus.completed) {
+      return;
+    }
+    setState(() => oldThemedImage = null);
+    _controller.value = 0;
+  }
+
+  void initState() {
+    super.initState();
+    theme = widget.theme;
+  }
+
+  void _triggerThemeChange() {
+    final renderBoundary = _boundaryKey.currentContext!.findRenderObject()
+        as RenderRepaintBoundary;
+    renderBoundary.toImage().then(_onImageReady);
+  }
+
+  void _onImageReady(ui.Image image) {
+    setState(() {
+      oldThemedImage = image;
+      theme = widget.theme;
+    });
+    _controller.forward(from: 0);
+  }
+
+  void didUpdateWidget(ShapeThemeSwitcher old) {
+    if (old.theme != widget.theme) {
+      WidgetsBinding.instance!
+          .addPostFrameCallback((_) => _triggerThemeChange());
+    }
+    super.didUpdateWidget(old);
+  }
+
+  Widget _body(BuildContext context) {
+    var bodyWithTargetTheme = Theme(
+      data: theme,
+      child: widget.child,
+    );
+    if (oldThemedImage == null) {
+      return bodyWithTargetTheme;
+    }
+    return AnimatedBuilder(
+      animation: _animation,
+      builder: (context, child) => Stack(
+        children: [
+          Positioned.fill(child: bodyWithTargetTheme),
+          Positioned.fill(
+              child: ClipPath(
+            clipper: PositionedShapeClipper(
+              widget.alignment,
+              _animation.value,
+              widget.borderTween.transform(_animation.value)!,
+            ),
+            child: child,
+          ))
+        ],
+      ),
+      child: IgnorePointer(
+        child: RawImage(
+          image: oldThemedImage,
+          scale: 1.0,
+        ),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return RepaintBoundary(
+      key: _boundaryKey,
+      child: _body(context),
+    );
+  }
+}
+
+class PositionedShapeClipper extends CustomClipper<Path> {
+  final Alignment origin;
+  final double t;
+  final ShapeBorder shape;
+
+  PositionedShapeClipper(
+    this.origin,
+    this.t,
+    this.shape,
+  );
+
+  @override
+  Path getClip(Size size) {
+    final originPoint = origin.alongSize(size);
+    final radius = [
+          size.topLeft(Offset.zero),
+          size.topRight(Offset.zero),
+          size.bottomLeft(Offset.zero),
+          size.bottomRight(Offset.zero),
+        ].fold<double>(
+          0,
+          (currRadius, p) => math.max(
+            currRadius,
+            (originPoint - p).distance,
+          ),
+        ) *
+        1.2;
+    final rectPath = Path()..addRect(Offset.zero & size);
+    final shapeRect = Rect.fromCircle(center: originPoint, radius: radius * t);
+    final shapePath = shape.getOuterPath(shapeRect);
+    return Path.combine(PathOperation.difference, rectPath, shapePath);
+  }
+
+  @override
+  bool shouldReclip(PositionedShapeClipper oldClipper) =>
+      t != oldClipper.t || origin != oldClipper.origin;
+}
